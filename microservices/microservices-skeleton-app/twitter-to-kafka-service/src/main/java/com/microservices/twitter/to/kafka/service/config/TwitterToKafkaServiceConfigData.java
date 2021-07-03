c72c7a95c816bd509451a3bcfe01d1418fa0ef37
Christos <cploutarchou@gmail.com>
2021-07-04 02:35:32 +0300

    Create Twitter To Kafka Service Application

---
+++ microservices/microservices-skeleton-app/twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
@@ -0,0 +1,15 @@
+package com.microservices.twitter.to.kafka.service.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfigData {
+    private List<String> twitterKeywords;
+    private String welcomeMessage;
+}
---
+++ microservices/microservices-skeleton-app/twitter-to-kafka-service/src/test/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.microservices.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+
+
+class TwitterToKafkaServiceApplicationTest {
+    @Test
+    public void contextLoad() {
+
+    }
+}

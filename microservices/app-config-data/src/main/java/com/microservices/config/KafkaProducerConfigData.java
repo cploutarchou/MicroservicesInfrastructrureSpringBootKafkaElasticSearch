5037e300f75ffdcb2c60fc7cc78f9b8c5136360e
Christos Ploutarchou <cploutarchou@gmail.com>
2021-09-07 22:02:42 +0300

    corrections

--- microservices/app-config-data/src/main/java/com/microservices/config/KafkaProducerConfigData.java
+++ microservices/app-config-data/src/main/java/com/microservices/config/KafkaProducerConfigData.java
@@ -16,6 +16,7 @@ public class KafkaProducerConfigData {
     private String compressionType;
     private String acks;
     private Integer batchSize;
+    private Integer batchSizeBoostFactor;
     private Integer lingerMs;
     private Integer requestTimeoutMs;
     private Integer retryCount;

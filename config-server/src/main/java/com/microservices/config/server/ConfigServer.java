9be5c0bdbae4db2f538b7afdbc0bb361f25687c1
Christos Ploutarchou <cploutarchou@gmail.com>
2021-09-14 21:32:06 +0300

    refactor

--- config-server/src/main/java/com/microservices/config/server/ConfigServer.java
+++ config-server/src/main/java/com/microservices/config/server/ConfigServer.java
@@ -2,7 +2,6 @@ package com.microservices.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer

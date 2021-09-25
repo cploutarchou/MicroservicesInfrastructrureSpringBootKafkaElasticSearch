2f8191cd4599de8b284ca386572eebbdaee8cdbf
Christos Ploutarchou <cploutarchou@gmail.com>
2021-09-25 04:33:54 +0300

    create elastic-index-client module

---
+++ elastic/elastic-index-client/src/main/java/com/microservices/elastic/index/client/utils/ElasticIndexUtils.java
@@ -0,0 +1,22 @@
+package com.microservices.elastic.index.client.utils;
+
+import com.microservices.elastic.model.index.IndexModel;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ElasticIndexUtils<T extends IndexModel> {
+
+    public List<IndexQuery> getIndexQueries(List<T> documents) {
+        return documents.
+                stream().map(document -> new IndexQueryBuilder()
+                        .withId(document.getId())
+                        .withObject(document)
+                        .build()
+                ).collect(Collectors.toList());
+    }
+}

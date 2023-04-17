d391fadbef17ee4d6ebdc75c8e925bae42b38405
Christos Ploutarchou <cploutarchou@gmail.com>
2023-04-17 22:45:05 +0300

    add support for mac , win and linux OS

--- docker/check-kafka-topics-created.sh
+++ docker/check-kafka-topics-created.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # check-kafka-topics-created.sh
 
-apt-get update -y
+# Check the operating system
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    apt-get update -y
+    yes | apt-get install kafkacat
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    brew update
+    yes | brew install kafkacat
+elif [[ "$OSTYPE" == "win"* ]]; then
+    # Windows
+    choco install kafkacat
+fi
 
-yes | apt-get install kafkacat
-
+# Check if the Kafka topic has been created
 kafkacatResult=$(kafkacat -L -b kafka-broker-1:9092)
 
 echo "kafkacat result:" $kafkacatResult
@@ -15,4 +26,5 @@ while [[ ! $kafkacatResult == *"twitter-topic"* ]]; do
   kafkacatResult=$(kafkacat -L -b kafka-broker-1:9092)
 done
 
+# Launch a Cloud Native Buildpack (CNB) lifecycle process
 ./cnb/lifecycle/launcher
